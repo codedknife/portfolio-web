bb789a78b488bbc6bbd12673cc10a0f6c4ab2d10
codedknife <tranash3@msu.edu>
2025-04-15 22:15:23 -0400

    refine mobile responsiveness

--- src/app/navigation.js
+++ src/app/navigation.js
@@ -7,27 +7,29 @@ export default function Navigation() {
 
     return (
     <>
-        {/* TOGGLE */}
-        <div className="sm:hidden flex justify-end mb-4 p-5">
+        <div className="sm:hidden fixed top-0 left-0 w-full z-50 bg-white">
+            {/* TOGGLE */}
+            <div className="flex justify-end p-5">
             <button
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
                 className="text-black text-2xl focus:outline-none cursor-pointer"
             >
                 {isMenuOpen ? "✕" : "☰"}
             </button>
-        </div>
+            </div>
 
-        {/* MOBILE MENU */}
-        {isMenuOpen && (
-        <nav className="sm:hidden mb-10">
-            <ul className="flex flex-col gap-4 text-center">
-                <li><a href="#home" className="nav-link">HOME</a></li>
-                <li><a href="#about" className="nav-link">ABOUT</a></li>
-                <li><a href="#experience" className="nav-link">EXPERIENCE</a></li>
-                <li><a href="#work" className="nav-link">WORK</a></li>
-            </ul>
-        </nav>
-        )}
+            {/* MOBILE MENU */}
+            {isMenuOpen && (
+            <nav className="pb-4">
+                <ul className="flex flex-col gap-4 text-center">
+                <li><a href="#home" className="nav-link" onClick={() => setIsMenuOpen(false)}>HOME</a></li>
+                <li><a href="#about" className="nav-link" onClick={() => setIsMenuOpen(false)}>ABOUT</a></li>
+                <li><a href="#experience" className="nav-link" onClick={() => setIsMenuOpen(false)}>EXPERIENCE</a></li>
+                <li><a href="#work" className="nav-link" onClick={() => setIsMenuOpen(false)}>WORK</a></li>
+                </ul>
+            </nav>
+            )}
+        </div>
 
         {/* DESKTOP MENU */}
         <nav className="navigation mb-10 hidden sm:block">
--- src/app/page.js
+++ src/app/page.js
@@ -21,16 +21,16 @@ export default function Home() {
         </div>
 
         {/* LANDING SECTION */}
-        <div id="home" className=" relative grid grid-cols-1 min-h-screen place-items-center">
+        <div id="home" className="relative grid grid-cols-1 h-screen overflow-hidden place-items-center px-1 md:px-4">
 
           {/* COL-1 */}
           <div className="relative justify-center flex flex-col">
             {/* greeting */}
-            <p className="greeting">HI, I'M</p>
+            <p className="text-[2rem] md:text-[3rem] text-left mt-25 md:mt-0 mb-5 md:mb-2">HI, I'M</p>
             {/* name */}
-            <h1 className="text-center name">ASHLEY TRAN.</h1>
+            <h1 className="text-[3rem] sm:text-xl md:text-[7.5rem] text-center name">ASHLEY TRAN.</h1>
             {/* specialization */}
-            <h2 className="blurb text-center"> WEB DEVELOPMENT + UX/UI</h2>
+            <h2 className="text-[1rem] mb-[2.5rem] md:text-[1.5rem] text-center"> WEB DEVELOPMENT + UX/UI</h2>
 
             {/* cta */}
             <div className="flex place-items-center grid">
@@ -40,20 +40,20 @@ export default function Home() {
               >
                 VIEW RESUME
               </a>
-              <div className="w-0 h-0 border-l-8 mt-25 border-r-8 border-b-0 border-t-8 border-transparent border-t-black"></div>
+              <div className="w-0 h-0 border-l-8 mt-25 md:mt-25 border-r-8 border-b-0 border-t-8 border-transparent border-t-black"></div>
             </div>
           </div>
 
         </div>
 
 
-
+        {/* ABOUT SECTION*/}
         <div className="min-h-screen">
-          <div className="relative grid grid-cols-1 md:grid-cols-2 pl-25 mb-10">
-            <div className="img-background"></div>
-            <div className="mr-25">
+          <div className="relative grid grid-cols-1 md:grid-cols-2 ml-15 mr-15 md:ml-25 md:mr-25 mb-5 md:mb-10">
+            <div className="img-background w-4/4 md:w-3/4"></div>
+            <div>
               <h2 id="about" className="section-heading text-center bold mb-5">ABOUT</h2>
-              <p className="text-base">
+              <p className="text-justify">
               I'm a front-end web developer with a degree in Computer Science and a minor in Business from Michigan State University, where I graduated in Spring 2024. 
               I’m passionate about building thoughtful, responsive, and accessible digital experiences that look great and feel intuitive to use.
               I specialize in front-end development and love working with modern frameworks and tools to bring ideas to life on the web. 
@@ -63,7 +63,7 @@ export default function Home() {
               </p>
               {/* tech stack */}
               <div className="mt-10">
-                <h3 className="tech-stack text-center bold">
+                <h3 className="text-center bold text-[1rem] md:text-[1.5rem]">
                   SKILLS AND SPECIALIZATIONS
                 </h3>
                 <ul className="flex flex-wrap justify-center gap-2 mt-4">
@@ -89,12 +89,13 @@ export default function Home() {
         </div>
   
 
-        <div className="min-h-screen mr-25 ml-25">
+         {/* EXPERIENCE SECTION*/}
+        <div className="min-h-screen relative relative grid grid-cols-1 ml-15 mr-15 md:ml-25 md:mr-25 mb-5 md:mb-10">
           <h2 id="experience" className="section-heading text-center bold mb-5">EXPERIENCE</h2>
           
           <ul className="text-center">
-            <div className="grid grid-cols-3 place-items-center">
-              <div className="text-background p-3 job-card min-w-100">
+            <div className="grid grid-cols-1 md:grid-cols-3 place-items-center mb-1">
+              <div className="text-background p-3 job-card w-4/4 h-4/4 mb-7 md:mb-0 grid place-items-center">
                 <li className="job-title">WEB CONTENT ADMINISTRATOR</li>
                 <div className="">
                   <p><i>MSU COLLEGE OF ENGINEERING</i></p>
@@ -102,19 +103,19 @@ export default function Home() {
                 </div>
               </div>
 
-              <div className="relative border-l-2 h-full">
+              <div className="relative border-l-2 h-full hidden md:block">
                 <div className="absolute left-1/2 top-1/2 transform -translate-x-5/5 h-0.5 w-25 bg-foreground"></div>
               </div>
 
               <div></div>
             </div>
 
-            <div className="grid grid-cols-3 place-items-center">
+            <div className="grid grid-cols-1 md:grid-cols-3 place-items-center">
               <div></div>
               <div className="relative border-l-2 h-full">
                 <div className="absolute left-1/2 top-1/2 transform h-0.5 w-25 bg-foreground"></div>
               </div>
-              <div className="text-background p-3 job-card min-w-100">
+              <div className="text-background p-3 job-card w-4/4 mb-5 md:mb-0">
                 <li className="job-title">UNDERGRADUATE LEARNING ASSISTANT</li>
                   <div className="">
                     <p><i>MSU COLLEGE OF ENGINEERING</i></p>
@@ -123,27 +124,27 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="grid grid-cols-3 place-items-center">
-              <div className="text-background p-3 job-card min-w-100">
+            <div className="grid grid-cols-1 md:grid-cols-3 place-items-center">
+              <div className="text-background p-3 job-card w-4/4 mb-5 md:mb-0">
                 <li className="job-title">STUDENT FRONT-END WEB ASSISTANT</li>
                 <div className="">
                   <p><i>MSU COLLEGE OF ENGINEERING</i></p>
                   <p>MAY 2023 - MAY 2024</p>
                 </div>
               </div>
-              <div className="relative border-l-2 h-full">
+              <div className="relative border-l-2 h-full hidden md:block">
                 <div className="absolute left-1/2 top-1/2 transform -translate-x-5/5 h-0.5 w-25 bg-foreground"></div>
               </div>
               <div></div>
             </div>
 
-            <div className="grid grid-cols-3 place-items-center">
+            <div className="grid grid-cols-1 md:grid-cols-3 place-items-center">
               <div>
               </div>
-              <div className="relative border-l-2 h-full">
+              <div className="relative border-l-2 h-full hidden md:block">
                 <div className="absolute left-1/2 top-1/2 transform h-0.5 w-25 bg-foreground"></div>
               </div>
-              <div className="text-background p-3 job-card min-w-100">
+              <div className="text-background p-3 job-card w-4/4 mb-8 md:mb-0">
                 <li className="job-title">INFORMATION TECHNOLOGY INTERN</li>
                   <div className="">
                     <p><i>NORTH AMERICAN LIGHTING</i></p>
@@ -152,27 +153,29 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="grid grid-cols-3 place-items-center">
-              <div className="text-background p-3 job-card min-w-100">
+            <div className="grid grid-cols-1 md:grid-cols-3 place-items-center">
+              <div className="text-background p-3 job-card w-4/4 h-4/4 mb-5 md:mb-0 grid place-items-center">
                 <li className="job-title">CASHIER</li>
                 <div className="">
                   <p><i>ASIA'S FINEST</i></p>
                   <p>DECEMBER 2019 - DECEMBER 2023</p>
                 </div>
               </div>
-              <div className="relative border-l-2 h-full">
+              <div className="relative border-l-2 h-full hidden md:block">
                 <div className="absolute left-1/2 top-1/2 transform -translate-x-5/5 h-0.5 w-25 bg-foreground"></div>
               </div>
-              <div></div>
+              <div className="hidden md:block"></div>
             </div>
           </ul>
         </div>
 
+
+
         <div className="min-h-screen">
         <h2 id="work" className="section-heading text-center bold mb-5">WORK</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
-        <div className="project-background ml-25"></div>
-          <div className="text-center mr-25">
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center ml-15 mr-15">
+        <div className="project-background hidden sm:block"></div>
+          <div className="text-center">
             <h3 className="project-name">VISIONARY PHOTOBOOTHS</h3>
             <h4>AUGUST 2024 - AUGUST 2024</h4>
             <ul className="flex flex-wrap justify-center gap-2 mt-4">
@@ -189,7 +192,7 @@ export default function Home() {
                     </li>
                   ))}
                 </ul>
-            <p className="mb-10 mt-10">
+            <p className="mb-10 mt-10 ">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
@@ -199,7 +202,7 @@ export default function Home() {
               href="https://visionarymi.com/"
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-block px-5 py-2 bg-black text-white rounded hover:bg-gray-800 transition"
+              className="inline-block px-5 py-2 bg-black text-white hover:bg-gray-800 transition"
             >
               LEARN MORE
             </a>
