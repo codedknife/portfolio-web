49aa3a0961ca421700f0f14a556e887621e42002
codedknife <tranash3@msu.edu>
2025-05-05 20:00:24 -0400

    create project card component

---
+++ src/app/project-card.js
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+
+const ProjectCard = ({ title, date, url, specialties, description }) => {
+  return (
+    <div className="w-1/1 md:w-[49.5%] pb-5 md:pb-0">
+       <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:bg-[#66615C]/10 transition h-150 md:h-125 bg-white border border-gray-300 p-5 flex flex-col items-center justify-center text-center"
+      >
+        <div>
+          <h2 className="text-2xl font-bold mb-4">{title}</h2>
+          <h4>{date}</h4>
+          <ul className="flex flex-wrap justify-center gap-2 mt-4">
+            {specialties.map((specialty) => (
+              <li
+                key={specialty}
+                className="bg-[#66615C] text-white text-xs font-medium px-3 py-1"
+              >
+                {specialty}
+              </li>
+            ))}
+          </ul>
+          <p className="p-5">{description}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default ProjectCard;
